c22032cc2a9a8e2bc6d99f454d9e3a16fbbdb1e5
threey-y <52376594+threey-y@users.noreply.github.com>
2019-07-14 17:02:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,20 @@
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
 // TODO write your code here...
-
+name = name.toUpperCase();
+console.log(name);
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+var arr = sentence.split(' ');
+//console.log(arr);
+for(var i = 0;i < arr.length;i++){
+    arr[i] = arr[i].charAt(0).toUpperCase()+arr[i].slice(1);
+}
+console.log(arr.join(' '));
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
-//TODO write your code here...
+var arr1 = money.substr(1,2);
+console.log(arr1);
